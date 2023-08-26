358dd2a51ac23a819495a63db20ed6b9e91b0b70
DanielMartinez2002 <98615675+DanielMartinez2002@users.noreply.github.com>
2023-08-26 16:00:06 -0600

    Se corrigio la grafica Weierstrass

--- Tarea_3/Weiestrass.py
+++ Tarea_3/Weiestrass.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def wierstrass(x, y, a, b, num_terms):
+def wierstrass(x, y, a, b, k_max):
     result = 0
-    for i in range(num_terms):
-        result += a**i * np.cos(2 * np.pi * b**i * (x + 0.5)) + a**i * np.cos(2 * np.pi * b**i * (y + 0.5))
+    for k in range(k_max):
+        result += (a**k * np.cos(2 * np.pi * b**k * (x + 0.5)) - a**k*np.cos(np.pi*b**k)) + (a**k * np.cos(2 * np.pi * b**k * (y + 0.5)) - a**k*np.cos(np.pi*b**k))
     return result
 
 # Crear una figura en 3D
@@ -13,12 +13,12 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Crear valores para x e y
-x = np.linspace(-2, 2, 100)
-y = np.linspace(-2, 2, 100)
+x = np.linspace(-0.5, 0.5, 150)
+y = np.linspace(-0.5, 0.5, 150)
 x, y = np.meshgrid(x, y)
 
 # Evaluar la función de Wierstrass en 3D
-z = wierstrass(x, y, a=0.5, b=3, num_terms=50)
+z = wierstrass(x, y, a=0.5, b=3, k_max=20)
 
 # Crear la gráfica de la función de Wierstrass en 3D
 ax.plot_surface(x, y, z, cmap='viridis')
@@ -27,7 +27,7 @@ ax.plot_surface(x, y, z, cmap='viridis')
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
-ax.set_title('Función de Weiestrass en 3D')
+ax.set_title('Función de Weierstrass en 3D')
 
 # Mostrar la gráfica
 plt.show()
