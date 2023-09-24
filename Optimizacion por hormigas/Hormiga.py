634d584d31acb78f8a287ac1bdccffd336ece92e
Daniel Martínez <daniel.martinez5659@alumnos.udg.mx>
2023-09-23 21:13:45 -0600

    Se puso la funcion step

--- Optimizacion por hormigas/Hormiga.py
+++ Optimizacion por hormigas/Hormiga.py
@@ -4,25 +4,26 @@
 Tarea 3 - Funciones de prueba para optimización multimodal, multiobjetivo y multidimensional
 
 """
-import numpy as np  # Importa la biblioteca NumPy para manipulación numérica
-import matplotlib.pyplot as plt  # Importa la biblioteca Matplotlib para visualización
+import numpy as np  
+import matplotlib.pyplot as plt  
 
-def Michalewicz(x,y):
-    return -((np.sin(x) * np.sin((1 * x**2) / np.pi)**(2*M)) + (np.sin(y) * np.sin((2 * y**2) / np.pi)**(2*M)))
-x = np.linspace(0, np.pi, 100)  # Crea 100 valores equidistantes en el rango 0 a pi
-y = np.linspace(0, np.pi, 100)
-x, y = np.meshgrid(x, y)  # Crea una malla de valores X e Y para usar en la gráfica
-M=10# Parámetro de ajuste para la función de Michalewicz
-z = Michalewicz(x,y)
+def Step(x,y):
+    return np.floor(x)**2+np.floor(y)**2
 
-fig = plt.figure()  # Crea una figura para la gráfica
-ax = fig.add_subplot(111, projection='3d')  # Agrega un subplot 3D a la figura
+x = np.linspace(-10, 10, 70)  # Crea 100 valores equidistantes en el rango -5 a 5
+y = np.linspace(-10, 10, 70)
+x, y = np.meshgrid(x, y) 
 
-surface = ax.plot_surface(x, y, z, cmap='viridis')  # Crea la gráfica de superficie con colores viridis
+z = Step(x,y)
 
-ax.set_xlabel('X')  # Agrega etiqueta al eje X
-ax.set_ylabel('Y')  # Agrega etiqueta al eje Y
-ax.set_zlabel('Z')  # Agrega etiqueta al eje Z
-ax.set_title('Gráfica de la función Michalewicz en 3D')  # Agrega un título a la gráfica
+fig = plt.figure() 
+ax = fig.add_subplot(111, projection='3d')  
 
-plt.show()  # Muestra la gráfica en una ventana emergente
+surface = ax.plot_surface(x, y, z, cmap='viridis')  
+
+ax.set_xlabel('X') 
+ax.set_ylabel('Y') 
+ax.set_zlabel('Z') 
+ax.set_title('Gráfica de la función Step')  
+
+plt.show() 
